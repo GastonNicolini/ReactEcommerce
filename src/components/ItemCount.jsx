6d1baad9219b9733a84d6a7593f4ce2253975d0e
Gaston Nicolini <gastonnicolini@gmail.com>
2023-02-11 02:24:56 -0300

    added toast warning for amount over stock available

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
-import { Text, Button, Container, Flex, Spacer } from '@chakra-ui/react'
+import { Text, Button, Container, Flex, Spacer, useToast, Wrap, WrapItem } from '@chakra-ui/react'
 
 const ItemCount = ({ stock }) => {
   const [amount, setAmount] = useState(0);
+  const toast = useToast()
+  const tooMuchId = 'tooMuchToast'
+
+  const stockWarningToast = (id, error) => {
+    if (!toast.isActive(id)) {
+      toast ({
+        id,
+        title: error,
+        status: 'warning',
+        isClosable: true,
+      })
+    }
+  }
 
   const remove = () => {
     setAmount (amount - 1);
@@ -17,20 +30,26 @@ const ItemCount = ({ stock }) => {
     return (
       <Container>
         <Flex alignItems='center' gap='2'>
-          <Button onClick={ remove }>-</Button>
+          <Button bg='#d7d7d7' onClick={ remove }>-</Button>
           <Text>{ amount }</Text>
-          <Button>+</Button>
+          <Button 
+          _hover={{ bg: '#f5f5f5' }}
+          bg='#f5f5f5'
+          onClick={ stockWarningToast(tooMuchId, 'No more stock available') }>+</Button>
         </Flex>
       </Container>
     )
   
-  } else if(amount == 0) {
+  } else if (amount == 0) {
     return (
       <Container>
         <Flex alignItems='center' gap='2'>
-          <Button>-</Button>
+          <Button
+          _hover={{ bg: '#f5f5f5' }}
+          bg='#f5f5f5'
+          >-</Button>
           <Text>{ amount }</Text>
-          <Button onClick={ add }>+</Button>
+          <Button bg='#d7d7d7' onClick={ add }>+</Button>
         </Flex>
       </Container>
     )
@@ -38,9 +57,9 @@ const ItemCount = ({ stock }) => {
     return (
       <Container>
         <Flex alignItems='center' gap='2'>
-          <Button onClick={ remove }>-</Button>
+          <Button bg='#d7d7d7' onClick={ remove }>-</Button>
           <Text>{ amount }</Text>
-          <Button onClick={ add }>+</Button>
+          <Button bg='#d7d7d7' onClick={ add }>+</Button>
         </Flex>
       </Container>
     )
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ItemCount from './ItemCount'
 
 const ItemDetail = ({ item }) => {
-  console.log(item)
   return (
     <Container className='body'>
       <div>
